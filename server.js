74821ae8cb0fceae48d5841a33b5bfb450f5e80d
Richard Astbury <richard.astbury@gmail.com>
2014-08-01 15:52:05 +0100

    added code to record use in table storage

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var dns = require('dns');
 var fs = require('fs');
 var ipcheck = require('./check-ip');
 var http = require('http');
+var uuid = require('uuid');
+var azure = require('azure');
+var tableClient;
+if (process.env.storage_account && process.env.storage_key){
+	tableClient = azure.createTableService(process.env.storage_account, process.env.storage_key);	
+	tableService.createTableIfNotExists('azurelookup', function(error){
+	    if(error) console.log(error);
+	});
+}
+
 
 http.createServer(function(req, res){
 	if (req.url === "/"){
@@ -20,9 +30,12 @@ http.createServer(function(req, res){
 }).listen(process.env.PORT || 8080);
 
 
-function checkIp(ip){
+function checkIp(ip, address){
 	for (var range in ips){
-		if (ipcheck(range)(ip)) return ips[range];
+		if (ipcheck(range)(ip)) {
+			save(address, ip, ips[range])
+			return ips[range];
+		}
 	}
 	return false;
 }
@@ -31,7 +44,7 @@ function checkDns(address, cb){
 	dns.resolve(address, function(err, addresses){
 		if (err) return cb(err);
 		for (var i = 0; i < addresses.length; i++){
-			var result = checkIp(addresses[i]);
+			var result = checkIp(addresses[i], address);
 			if (result) return cb(null, result);
 		}
 		cb(null, false);
@@ -53,9 +66,29 @@ function isIp(address){
 
 function check(address, cb){
 	if (isIp(address)){
-		return cb(null, checkIp(address));
+		return cb(null, checkIp(address, address));
 	}
 	checkDns(address, cb);
 }
 
+function save(address, ip, dc){
+
+	function newId(){
+		var date = new Date();
+		return ("00000000000000" + date.getTime()).substr(-14) + '~' + uuid.v4();
+	}
+	var id = newId();
+
+	if (!tableClient) return;
+	var entity = {
+		address : address,
+		ip : ip,
+		dc : dc,
+		PartitionKey : id.substr(0,10),
+		RowKey : id + "~" + uuid.v4(),
+	};
+	tableClient.insertEntity('azurelookup', entity, function(err){
+		if (err) console.log(err);
+	});
+}
 
