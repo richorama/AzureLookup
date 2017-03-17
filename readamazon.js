6cf01609a8da706a0b1641d6c6cf3a580cdb46aa
Richard Astbury <richard.astbury@gmail.com>
2017-03-17 14:10:54 +0000

    update AWS ranges. closes #1

--- readamazon.js
+++ readamazon.js
@@ -1,19 +1,6 @@
 var fs = require('fs');
-var data = fs.readFileSync('amazon.txt').toString().split("\r\n");
+var data = JSON.parse(fs.readFileSync('aws-ip-ranges.json').toString());
 
-var first = true;
-var lastBlank = true;
-var dc = "";
-var output = {};
-for (var i = 0; i < data.length; i++){
-	var line = data[i];
-	if (first){
-		dc = "Amazon - " + line.replace(":", "");
-	} else if (line !== ""){
-		output[line.split(" ")[0]] = dc;
-	}
-
-	first = (line === "" && lastBlank);
-	lastBlank = (line === "");
-}
-console.log(output);
+data.prefixes.forEach(record => {
+	console.log(`'${record.ip_prefix}' : 'Amazon - ${record.service} - ${record.region}',`);
+});
